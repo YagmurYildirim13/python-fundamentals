ed6fc992e7def8e3cc3bf021564479e4581ebeaa
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-01 01:13:18 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,18 @@
+#print("hello\nworld")
+
+#print("hello\tworld")
+
+#message = "hello world"
+#print(message)
+
+message = "hello"
+message1 = "WORLD"
+
+#print(message + " " + message1)
+
+#print(message[0:5])
+#python'da tam dilimleme formülü her zaman şöyledir:[başlangıç : bitiş : adım_sayısı]]
+
+print(message[0:5])
+
+
