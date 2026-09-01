081ffae06264abf3437f790688446b6d7f8b17ff
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-01 14:42:36 +0300

    Add files via upload

---
+++ numbers_and_types.py.py
@@ -0,0 +1,58 @@
+#integer, float ve type fonksiyonları
+
+#matematiksel işlemler
+#toplama  "+"
+#çıkarma "-"
+#çarpma "*"
+#bölme "/"
+#tam sayı bölme "//"
+#kuvvet alma "**"
+#mutlak değer "abs"
+#yuvarlama "round"
+#işlem önceliği: parantez, çarpma ve bölme, toplama ve çıkarma
+
+
+
+
+
+
+#sayi1 = 5
+#sayi2 = 3.8
+
+#print(sayi1)
+#print(type(sayi1))
+#print(type(sayi2))
+
+
+#print(8+4)
+#print(16/5)
+#print(15//4)
+#print(2**5)
+#print(abs(-5))
+
+#sayi1 = 3.23802923
+#int(round(sayi1))
+#rint(round(sayi1,3))
+
+#karşilaştirma operatörleri
+#eşittir "=="
+#eşit değildir "!"
+#küçüktür "<"
+# büyüktür ">"
+# küçük eşittir "<="
+# büyük eşittir ">="
+
+#print(3==3)
+#print(5==2)
+
+#sayi1 = 7
+#sayi2 = 10
+
+#print(sayi1<sayi2)
+
+#strig ve int dönüşümleri
+
+#sayi1 = "100"
+#sayi2 = 100
+#sayi3 = int(sayi1)
+#print(sayi2 == sayi3)
