be8e9e78acc8063699afd731012b5d81d9b5924a
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-06 03:34:39 +0300

    Add files via upload

---
+++ lists.py.py
@@ -0,0 +1,66 @@
+
+#renkler = ["siyah","beyaz","sarı", "mavi", "yeşil"]
+# print(renkler)
+
+#append metodu
+#insert metodu
+#remove metodu
+#pop metodu 
+#reverse metodu
+#sort and sorted metotları
+
+
+#renkler = ["siyah","beyaz","sarı", "mavi", "yeşil"]
+
+#renkler.append("gri") #appent metodu ile listenin sonuna yeni bir eleman yüklenirprint(renkler)
+
+#renkler.insert(0,"gri") #insert metodu ile listenin istediğimiz bir indexine yeni bir eleman eklenir
+
+#renkler.remove("sarı") #remove metodu ile listeden istediğimiz bir elemanı silebiliriz
+
+#renkler2 = ["turuncu", "pembe"]
+
+#renkler.extend(renkler2) #extend metodu ile bir listeyi başka bir listenin sonuna eklyebiliriz
+
+#renkler.pop() #pop metodu ile listenin son elemanını siler
+
+#renkler.reverse() #reverse metodu ile listenin elemanlarının sırasını tersine çevirir
+
+#renkler.sort() #sort metodu ile listenin elemanlarını alfabetik olarak sıralar
+
+#renkler.sort(reverse=True) #sort metodu ile listenin elemanlarını alfabetik olarak tersine sıralar
+
+#liste2 = sorted(renkler) #sorted metodu ile listenin elemanlarını alfabetik olarak sıralar ve yeni bir liste oluşturur
+
+#print(renkler)
+#print(liste2)
+
+#min, max ve in kullanımı
+#sum ve kullanımı
+#for döngüsü ile listeyi yazdırmak
+#enemerate
+#listeyi stringe çevirmek ve join metodu
+#stringi listeye çevirmek ve split metodu
+
+renkler = ["siyah","beyaz", "sarı", "mavi","yeşil"]
+
+#sayilar = [1,2,3,4,5]
+
+# print(min(renkler))
+# print(max(renkler))
+# print(min(sayilar))
+# print(max(sayilar))
+# print(sum(sayilar))
+
+#for renk in renkler:
+    #print(renk)
+
+#print(list(enumerate(renkler))) #enumarate metodu ile listenin indexlerini ve elemanlarını birlikte yazdırabiliriz
+
+#print("Gri" in renkler) #inmetodu ile listenin içinde istediğimiz bir elemanın olup olmadığını kontrol edebiliriz
+
+#stringrenkler ="-".join(renkler)
+#print(stringrenkler) #join metodu ile listenin elemanlarını birleştirip stringe çevirebiliriz
+#print(stringrenkler)
+#renkler2 = stringrenkler.split("-")
+#print(renkler2)
