c7d66516adcd5f581bd69d07712bc3a269c26ed0
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-12 12:23:47 +0300

    Add files via upload

---
+++ if_else_elif.py.py
@@ -0,0 +1,59 @@
+#a = 5
+#b = 7
+#if a == b:
+    #print("a" == "b")
+
+#a = 10
+#b = 5
+#if a > b:
+    #print("a > b")
+
+#a = 6
+#b = 8
+#if a == b:
+    #print("a == b")
+#else:
+    #print("a != b")
+
+#renk= "Siyah"
+#if renk == "Beyaz":
+    #print("Renk Beyaz")
+#elif renk == "Sarı":
+    #print("Renk Sarı")
+#elif renk == "Mavi":
+    #print("Renk Mavi")
+#else:
+    #print("Hiçbir renk eşleşmedi")
+
+#a = 5
+#b = 8
+#c = 10
+#if a < b or c > a:
+    #print("koşul doğru")
+#else:
+    #print("koşul yanluış")
+
+#list = [1,2,3,4,6,7,8,9,10]
+
+#a = 4
+#if a in list:
+    #print("a listede var")
+#else:
+    #print("a listede yok")
+
+#a = 8
+#b = 10
+
+#if not a == b:
+    #print("koşul doğru")
+#else:
+    #print("koşul yanlış")
+
+#a= "python"
+#b = "pytho"
+#b += "n"
+
+#if a is b:
+    #print("a = b")
+#else:
+    #print("a != b")
