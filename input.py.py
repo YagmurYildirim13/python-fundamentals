3afb0947a87eb81ffc6121b6843606df3cf3e784
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-14 16:09:05 +0300

    Add files via upload

---
+++ input.py.py
@@ -0,0 +1,108 @@
+#sayi = input("Bir sayı giriniz:")
+#print(sayi)
+
+#sayi = int(input("Bir sayı giriniz: "))
+#print(sayi + 5)
+
+#sayi = int(input("Bir sayı giriniz"))
+
+#faktoriyel = 1
+
+#for i in range(1, sayi + 1):
+    #faktoriyel *= i
+#print(f"{sayi}! = {faktoriyel}")
+
+#sayi = int(input("Bir sayı giriniz"))
+
+#faktoriyel = 1
+#i = 2
+#while i <= sayi:
+    #faktoriyel *= i
+    #i += 1
+
+#print(f"{sayi}! = {faktoriyel}")
+
+#sayi = int(input("Bir sayı giriniz"))
+
+#prime = True
+#for i in range(2, sayi):
+    #if sayi %i == 0:
+        #prime = False
+        #break
+#if prime == True:
+    #print(f"{sayi} sayısı asaldır")
+#else:
+    #print(f"{sayi} sayısı asal değildir")
+
+#sayi = int(input("Bir sayı giriniz:"))
+
+#bolen_sayisi = 0
+
+#for i in range(1, sayi +1):
+    #if sayi % i == 0:
+        #bolen_sayisi += 1
+#print(bolen_sayisi)
+
+#sayi = int(input("Bir sayı giriniz"))
+
+#str_sayi = str(sayi)
+#toplam = 0
+
+#for rakam in str_sayi:
+    #toplam += int(rakam)
+#print(toplam)
+
+#sayi = int(input("Bir sayı giriniz"))
+
+#toplam = 0
+#gecici_sayi  = sayi
+
+#while gecici_sayi != 0:
+    #basamak = gecici_sayi % 10
+    #toplam += basamak
+    #gecici_sayi //= 10
+#print(toplam)
+
+#liste = []
+ #for i in range(5)
+   #sayi = int(input("Bir sayı giriniz"))
+   #liste.append(sayi)
+#print(f"en büyük : {max(liste)}")
+#print(f"en küçük sayı : {min(liste)}")
+
+
+# sayi = int(input("Bir sayı giriniz"))
+
+# karekök = sayi ** 0.5
+
+# if karekök == int(karekök):
+#     print("Tamkare")
+# else:
+#     print("Tamkare değildir")
+
+
+# metin = input("Bir metin giriniz: ")
+
+# sozluk = dict()
+
+# for harf in metin:
+#     if harf in sozluk:
+#         sozluk[harf] += 1
+#     else:
+#         sozluk[harf] = 1
+# for harf,adet in sozluk.items():
+#     print(harf,adet)
+
+
+ 
+
+      
+ 
+
+    
+    
+
+
+ 
+
+        
