a2e1135cea2000608aac16f331c94d14fa7b8af9
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-15 14:01:47 +0300

    Add files via upload

---
+++ exercise.py.py
@@ -0,0 +1,39 @@
+ #prime_list = list()
+
+#prime_list.append(2)
+
+#sayi = 3
+
+#while True:
+    #prime = True
+    #for i in range(2,sayi):
+        #if sayi %i == 0:
+            #prime = False
+            #break
+    #if prime:
+        #prime_list.append(sayi)
+        #if len(prime_list) == 100:
+            #break
+    #sayi += 1
+
+
+# liste = []
+# for sayi in range(100,1000):
+#     toplam = 0
+#     gecici_sayi = sayi
+#     while gecici_sayi != 0:
+#         basamak = gecici_sayi %10
+#         toplam += basamak ** 3
+#         gecici_sayi //= 10
+#     if toplam == sayi:
+#         liste.append(sayi)
+# print(liste)
+
+       
+
+
+
+
+
+
+
