356ccc9b10b486dce74d3c0315e970089bfedabc
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-17 02:23:59 +0300

    Add files via upload

---
+++ functions.py.py
@@ -0,0 +1,62 @@
+#def bilgi_ver():
+    #print("İşlem başarılı!")
+    #print("Bloğun içerisi")
+
+#bilgi_ver()
+
+#def selamla(isim):
+    #print("Merhaba " + isim)
+
+#selamla("Ali")
+
+# def topla(x,y):
+#     print(f"x + y = {x + y}")
+
+# topla(3, 6)
+
+#def ortalama_hesapla(liste):
+    #toplam = sum(liste)
+    #adet = len(liste)
+    #ortalama = toplam / adet
+    #print(f"Girilen sayıların ortalaması : {ortalama}")
+
+#ortalama_hesapla([1,2,3,4,5,6,7])
+
+#def buyuk_harfe_cevir(metin):
+    #metin = metin.upper()
+    #print(metin)
+
+#buyuk_harfe_cevir("yagmur yildirim")
+
+#def selamla(mesaj , isim = "Anonim"):
+    #print(f"{mesaj} {isim}.")
+
+#selamla("Merhaba")
+
+# ortalama_hesapla(x,y):
+    #return (x + y) / 2
+
+#a = ortalama_hesapla(2, 6)
+#b = ortalama_hesapla(6,10)
+#print(a + b)
+
+#def buyuk_harfe_cevir(metin):
+    #return metin.upper()
+
+#a = buyuk_harfe_cevir("cupcake")
+#print(a)
+
+    
+
+    
+
+    
+
+
+
+
+
+
+    
+
+ 
