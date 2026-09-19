9d0e82b37db45175e7f00f1396ee9476a04f83dd
Yağmur Yıldırım <yagmuryildirim1312@gmail.com>
2026-09-19 13:45:46 +0300

    Add files via upload

---
+++ modules.py.py
@@ -0,0 +1,20 @@
+#import math
+
+#sonuc = math.factorial(4)
+#print(sonuc)
+
+#sonuc = math.sqrt(65)
+#print(sonuc)
+
+#from math import factorial
+
+#sonuc = factorial(5)
+#print(sonuc)
+
+#from math import *
+
+#import os
+#import time
+#import datatime
+
+
